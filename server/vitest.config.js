302a05bacf7c3870098c97880cebc2ca0cd20578
WanderlustCoder <weremittens@gmail.com>
2026-03-30 17:45:44 -0600

    fix: include plugin test directories in test config

--- server/vitest.config.js
+++ server/vitest.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     testTimeout: 15000,
     hookTimeout: 15000,
     teardownTimeout: 5000,
-    include: ['tests/**/*.test.js'],
+    include: ['tests/**/*.test.js', 'plugins/**/tests/**/*.test.js'],
     exclude: ['**/node_modules/**', '**/dist/**', 'tests/test-container.js'],
     setupFiles: ['tests/worker-setup.js'],
     pool: 'forks',
